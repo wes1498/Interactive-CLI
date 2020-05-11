7242df9e7d6bf9338078aeab727ba4d4eba46934
Wesley Sequeira <wesleysequeira@Wesleys-MacBook-Pro.local>
2020-05-10 20:05:52 -0700

    tilda cmd prompt

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `blue`,
             textDecoration: `none`,
           }}
         >
